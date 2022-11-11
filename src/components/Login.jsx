54ff7cfef488ca3508a65950a5fc78c6b9bfbb22
saulmarte <saulkjh1227@gmail.com>
2022-11-11 18:00:56 -0400

    more little changes

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -23,14 +23,14 @@ const Login = ({ user, setisLogged }) => {
     return (
         <section className="login__container">
             <form onSubmit={handleSubmit} className='login'>
-                <h1 className="tittle__sign-In margin-auto">Sign In</h1>
+                <h1 className="tittle__sign-In margin-auto">Log In</h1>
                 <div className="mb-3">
-                    <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder='enter your email' />
+                    <label htmlFor="exampleInputEmail1" className="form-label">Correo</label>
+                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder='Ingrese su Correo' />
                 </div>
                 <div className="mb-3">
-                    <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-                    <input type="password" className="form-control" id="exampleInputPassword1" placeholder='enter your password' />
+                    <label htmlFor="exampleInputPassword1" className="form-label">Contraseña</label>
+                    <input type="password" className="form-control" id="exampleInputPassword1" placeholder='Ingrese su contraseña' />
                 </div>
                 <button type="submit" className="btn btn-dark btn-login">Submit</button>
             </form>
